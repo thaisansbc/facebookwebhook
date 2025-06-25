806777f758422f98084718f738f493ac8141b62a
thaisan un <thaisan.sbc@gmail.com>
2025-06-25 14:38:57 +0700

    add file index

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+$my_verify_token = "2576453712648736HIP";
+
+
+
+$challenge = $_GET['hub_challenge'];
+$verify_token = $_GET['hun_verify_token'];
+if($my_verify_token === $verify_token){
+	echo $challenge;
+	exit();
+}
+
+?>
